1931300520034a9992f0499c938c61afdacfd0cc
Unstoppable1226 <joel.10joel.10@gmail.com>
2017-07-04 21:07:43 +0200

    Mise en place de l'interface de la page d'acceuil et Intégration de Jquery
    Déclaration de jquery dans le projet afin de pouvoir l'utiliser comme je
    le souhaite tout au long du projet.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+declare var $:any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'GraphToLearn';
+	title = 'GraphToLearn';
+	content = [
+		{ title: 'Andorra' },
+		{ title: 'United Arab Emirates' },
+		{ title: 'Afghanistan' },
+		{ title: 'Antigua' },
+		{ title: 'Anguilla' },
+		{ title: 'Albania' },
+		{ title: 'Armenia' },
+		{ title: 'Netherlands Antilles' },
+		{ title: 'Angola' },
+		{ title: 'Argentina' },
+		{ title: 'American Samoa' },
+		{ title: 'Austria' },
+		{ title: 'Australia' },
+		{ title: 'Aruba' },
+		{ title: 'Aland Islands' },
+		{ title: 'Azerbaijan' },
+		{ title: 'Bosnia' },
+		{ title: 'Barbados' },
+		{ title: 'Bangladesh' },
+		{ title: 'Belgium' },
+		{ title: 'Burkina Faso' },
+		{ title: 'Bulgaria' },
+		{ title: 'Bahrain' },
+		{ title: 'Burundi' }
+	];
+	ngAfterViewInit(){
+		$('.ui.search').search({
+		    	source: this.content
+		});
+	}
+	search() {
+		this.title = 'Nouveau titre';
+	}
 }
